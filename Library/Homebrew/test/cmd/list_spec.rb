2f3806a47dda2d926e9a1397455c9dcc7b72e56e
Andreas Scherer <andreas_github@freenet.de>
2024-12-25 10:36:16 +0100

    Partial solution for cask processing.
    'brew list --cask' seems to work, but both 'brew list --cask --version'
    and 'brew list --version' fail because of empty cask list.

--- Library/Homebrew/test/cmd/list_spec.rb
+++ Library/Homebrew/test/cmd/list_spec.rb
@@ -13,19 +13,19 @@ RSpec.describe Homebrew::Cmd::List do
     formulae.each do |f|
       (HOMEBREW_CELLAR/f/"1.0/somedir").mkpath
     end
-    # casks.each do |f|
-    #   (HOMEBREW_CELLAR/"../Caskroom"/f/"42.0/somedir").mkpath
-    # end
+    casks.each do |f|
+      (HOMEBREW_PREFIX/"Caskroom"/f/"42.0/somedir").mkpath
+    end
 
     expect { brew "list", "--formula" }
       .to output("#{formulae.sort.join("\n")}\n").to_stdout
       .and not_to_output.to_stderr
       .and be_a_success
 
-    # expect { brew "list", "--cask" }
-    #   .to output("#{casks.sort.join("\n")}\n").to_stdout
-    #   .and not_to_output.to_stderr
-    #   .and be_a_success
+    expect { brew "list", "--cask" }
+      .to output("#{casks.sort.join("\n")}\n").to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
 
     expect { brew "list", "--formula", "--version" }
       .to output("#{formulae.sort.map { |name| "#{name} 1.0" }.join("\n")}\n").to_stdout
@@ -38,8 +38,11 @@ RSpec.describe Homebrew::Cmd::List do
     #   .and be_a_success
 
     # expect { brew "list", "--version" }
-    #   .to output("#{formulae.sort.map { |name| "#{name} 1.0" }.join("\n")}\n" +
-    #              "#{casks.sort.map { |name| "#{name} 42.0" }.join("\n")}\n").to_stdout
+    #   .to output(<<~OUTPUT,
+    #     #{formulae.sort.map { |name| "#{name} 1.0" }.join("\n")}
+    #     #{casks.sort.map { |name| "#{name} 42.0" }.join("\n")}
+    #   OUTPUT
+    #             ).to_stdout
     #   .and not_to_output.to_stderr
     #   .and be_a_success
   end
