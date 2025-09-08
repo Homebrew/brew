22bf2f96c4cd93912ea1130d27e3800f6c172e19
Douglas Eichelberger <d@eic.email>
2025-09-08 11:22:06 -0700

    Fix rebase errors

--- Library/Homebrew/dev-cmd/bottle.rb
+++ Library/Homebrew/dev-cmd/bottle.rb
@@ -410,7 +410,7 @@ module Homebrew
 
         bottle_tag, rebuild = if local_bottle_json
           _, tag_string, rebuild_string = Utils::Bottles.extname_tag_rebuild(formula.local_bottle_path.to_s)
-          [tag_string.to_sym, rebuild_string.to_i]
+          [T.must(tag_string).to_sym, rebuild_string.to_i]
         end
 
         bottle_tag = if bottle_tag
@@ -861,8 +861,7 @@ module Homebrew
       end
 
       sig {
-        params(formula: Formula, formula_ast: Utils::AST::FormulaAST,
-               bottle_hash: T::Hash[String, T.untyped])
+        params(formula: Formula, formula_ast: Utils::AST::FormulaAST, bottle_hash: T::Hash[String, T.untyped])
           .returns(T.nilable(T::Array[T::Hash[Symbol, T.any(String, Symbol)]]))
       }
       def old_checksums(formula, formula_ast, bottle_hash)
