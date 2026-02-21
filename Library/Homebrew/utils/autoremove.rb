fc6dd3157e9f7ca75a6fa032df887e63f2baabf6
ini <wu2196674@icloud.com>
2026-02-21 12:43:00 +0000

    include comments to clarify autoremove logic

--- Library/Homebrew/utils/autoremove.rb
+++ Library/Homebrew/utils/autoremove.rb
@@ -1,6 +1,8 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "installed_dependents"
+
 module Utils
   # Helper function for finding autoremovable formulae.
   #
@@ -37,6 +39,7 @@ module Utils
           # Include current runtime dependencies to align with brew uninstall
           formulae_to_keep += formula.installed_runtime_formula_dependencies
 
+          # Also include runtime deps from the current formula definition.
           formulae_to_keep += formula.runtime_dependencies(read_from_tab: false,
                                                            undeclared:    false).filter_map do |dep|
             dep.to_formula
