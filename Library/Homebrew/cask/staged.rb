1df500c8034fc01c910d06437b45d05b161f9b3f
Tilak Dave <tilak.dave@workato.com>
2026-03-05 23:59:53 +0530

    shell completions for casks

--- Library/Homebrew/cask/staged.rb
+++ Library/Homebrew/cask/staged.rb
@@ -1,6 +1,7 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "utils/popen"
 require "utils/user"
 require "utils/output"
 
@@ -33,7 +34,116 @@ module Cask
                             sudo: true)
     end
 
+    sig {
+      params(
+        commands:               T.any(Pathname, String),
+        base_name:              T.nilable(String),
+        shell_parameter_format: T.nilable(T.any(Symbol, String)),
+        shells:                 T::Array[Symbol],
+      ).void
+    }
+    def generate_completions_from_executable(*commands,
+                                             base_name: nil,
+                                             shell_parameter_format: nil,
+                                             shells: default_completion_shells(shell_parameter_format))
+      executable = commands.first.to_s
+      base_name ||= cask.token
+
+      config = cask.config
+      completion_script_path_map = {
+        bash: config.bash_completion/base_name,
+        zsh:  config.zsh_completion/"_#{base_name}",
+        fish: config.fish_completion/"#{base_name}.fish",
+        pwsh: config.pwsh_completion/"_#{base_name}.ps1",
+      }
+
+      shells.each do |shell|
+        popen_read_env = { "SHELL" => shell.to_s }
+        script_path = completion_script_path_map[shell]
+        next if script_path.nil?
+
+        shell_parameter = completion_shell_parameter(
+          shell_parameter_format,
+          shell,
+          executable,
+          popen_read_env,
+        )
+
+        popen_read_args = %w[]
+        popen_read_args << commands
+        popen_read_args << shell_parameter if shell_parameter.present?
+        popen_read_args.flatten!
+
+        popen_read_options = {}
+        popen_read_options[:err] = :err unless ENV["HOMEBREW_STDERR"]
+
+        script_path.dirname.mkpath
+        script_path.write ::Utils.safe_popen_read(popen_read_env, *popen_read_args, **popen_read_options)
+      end
+    end
+
+    sig { params(base_name: String).void }
+    def remove_generated_completions(base_name)
+      config = cask.config
+
+      [
+        config.bash_completion/base_name,
+        config.zsh_completion/"_#{base_name}",
+        config.fish_completion/"#{base_name}.fish",
+        config.pwsh_completion/"_#{base_name}.ps1",
+      ].each do |path|
+        path.delete if path.exist?
+      end
+    end
+
     private
+
+    sig { params(format: T.nilable(T.any(Symbol, String))).returns(T::Array[Symbol]) }
+    def default_completion_shells(format)
+      case format
+      when :cobra, :typer
+        [:bash, :zsh, :fish, :pwsh]
+      else
+        [:bash, :zsh, :fish]
+      end
+    end
+
+    sig {
+      params(
+        format:     T.nilable(T.any(Symbol, String)),
+        shell:      Symbol,
+        executable: String,
+        env:        T::Hash[String, String],
+      ).returns(T.nilable(T.any(String, T::Array[String])))
+    }
+    def completion_shell_parameter(format, shell, executable, env)
+      shell_parameter = (shell == :pwsh) ? "powershell" : shell.to_s
+
+      case format
+      when nil
+        shell_parameter
+      when :arg
+        "--shell=#{shell_parameter}"
+      when :clap
+        env["COMPLETE"] = shell_parameter
+        nil
+      when :click
+        prog_name = File.basename(executable).upcase.tr("-", "_")
+        env["_#{prog_name}_COMPLETE"] = "#{shell_parameter}_source"
+        nil
+      when :cobra
+        ["completion", shell_parameter]
+      when :flag
+        "--#{shell_parameter}"
+      when :none
+        nil
+      when :typer
+        env["_TYPER_COMPLETE_TEST_DISABLE_SHELL_DETECTION"] = "1"
+        ["--show-completion", shell_parameter]
+      else
+        "#{format}#{shell}"
+      end
+    end
 
     sig { params(paths: Paths).returns(T::Array[Pathname]) }
     def remove_nonexistent(paths)
