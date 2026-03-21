af4d98b7666316b8aaeabb3390898923b259108c
Sanket Kalekar <kalekarsanket005@gmail.com>
2026-03-21 09:29:26 +0000

    dev-cmd: inline helpers, add opoo warning, add basic spec

---
+++ Library/Homebrew/test/dev-cmd/which-entry_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "cmd/shared_examples/args_parse"
+require "dev-cmd/which-entry"
+
+RSpec.describe Homebrew::DevCmd::WhichEntry do
+  it_behaves_like "parseable arguments"
+end
