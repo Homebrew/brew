8dcb4fea4a263f1e64c67414563d04d07b31ed27
Sanket Kalekar <kalekarsanket005@gmail.com>
2026-05-02 07:58:29 +0000

    dev-cmd: fix which-entry spec to use integration test helper

--- Library/Homebrew/test/dev-cmd/which-entry_spec.rb
+++ Library/Homebrew/test/dev-cmd/which-entry_spec.rb
@@ -1,3 +1,4 @@
+# typed: false
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
@@ -6,7 +7,9 @@ require "dev-cmd/which-entry"
 RSpec.describe Homebrew::DevCmd::WhichEntry do
   it_behaves_like "parseable arguments"
 
-  it "raises a UsageError if --output-db is not passed" do
-    expect { brew "which-entry", "wget" }.to output(/`--output-db` is required/).to_stderr
+  it "raises a UsageError if --output-db is not passed", :integration_test do
+    expect { brew "which-entry", "wget" }
+      .to output(/`--output-db` is required/).to_stderr
+      .and be_a_failure
   end
 end
