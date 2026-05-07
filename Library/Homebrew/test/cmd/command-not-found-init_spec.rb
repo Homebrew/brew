97c61ae7c1d2cc61a4d1b7973f8f54702ba1f32c
bittoby <218712309+bittoby@users.noreply.github.com>
2026-05-07 17:05:39 +0200

    test/cmd/command-not-found-init: add integration test

--- Library/Homebrew/test/cmd/command-not-found-init_spec.rb
+++ Library/Homebrew/test/cmd/command-not-found-init_spec.rb
@@ -1,4 +1,4 @@
-# typed: strict
+# typed: false
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
@@ -6,4 +6,11 @@ require "cmd/command-not-found-init"
 
 RSpec.describe Homebrew::Cmd::CommandNotFoundInit do
   it_behaves_like "parseable arguments"
+
+  it "prints a handler script when output is not connected to a tty", :integration_test do
+    expect { brew "command-not-found-init" }
+      .to output(/.+/).to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
 end
