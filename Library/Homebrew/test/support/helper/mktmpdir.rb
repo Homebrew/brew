65cd23026b70aabcf082ace24102b3fccb902cb6
Bo Anderson <mail@boanderson.me>
2026-05-22 16:37:37 +0100

    Update mktmpdir test helper to work with Sorbet

--- Library/Homebrew/test/support/helper/mktmpdir.rb
+++ Library/Homebrew/test/support/helper/mktmpdir.rb
@@ -1,12 +1,20 @@
-# typed: false
+# typed: strict
 # frozen_string_literal: true
 
 module Test
   module Helper
     module MkTmpDir
-      def mktmpdir(prefix_suffix = nil)
+      extend T::Sig
+
+      sig do
+        params(
+          prefix_suffix: T.nilable(T.any(String, T::Array[String])),
+          blk: T.nilable(T.proc.params(tmpdir: Pathname).returns(T.untyped)),
+        ).returns(T.untyped)
+      end
+      def mktmpdir(prefix_suffix = nil, &blk)
         new_dir = Pathname.new(Dir.mktmpdir(prefix_suffix, HOMEBREW_TEMP))
-        return yield new_dir if block_given?
+        return blk.call(new_dir) if blk
 
         new_dir
       end
